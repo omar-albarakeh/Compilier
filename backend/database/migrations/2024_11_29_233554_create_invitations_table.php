9005b460010be9ef65d93dc9e6dc36417a3969f3
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-30 01:39:13 +0200

    laravel <> invitations table updated

--- backend/database/migrations/2024_11_29_233554_create_invitations_table.php
+++ backend/database/migrations/2024_11_29_233554_create_invitations_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
-        Schema::create('invitations', function (Blueprint $table) {
+         Schema::create('invitations', function (Blueprint $table) {
             $table->id();
+            $table->string('recipient_email', 45);
+            $table->foreignId('workspaces_id')->constrained('workspaces')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('invitations');
