61abf9ddb58a0406d369f1fbd261f727fdba4c30
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-30 01:39:42 +0200

    laravel <> files table updated

--- backend/database/migrations/2024_11_29_233613_create_files_table.php
+++ backend/database/migrations/2024_11_29_233613_create_files_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 45);
+            $table->string('path', 45);
+            $table->foreignId('workspaces_id')->constrained('workspaces')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('files');
