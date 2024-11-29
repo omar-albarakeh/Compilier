0dfa005bbf3d292e9e18d942d167d32533d06052
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-30 01:40:22 +0200

    laravel <> collaborations table updated

--- backend/database/migrations/2024_11_29_233631_create_collaborations_table.php
+++ backend/database/migrations/2024_11_29_233631_create_collaborations_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
         Schema::create('collaborations', function (Blueprint $table) {
             $table->id();
+            $table->string('role', 45);
+            $table->foreignId('users_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('workspaces_id')->constrained('workspaces')->onDelete('cascade');
+            $table->foreignId('invitations_id')->nullable()->constrained('invitations')->onDelete('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('collaborations');
