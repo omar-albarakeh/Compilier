1f56d6d5abf257949fba481c14f951ef0a3319f5
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-01 01:47:09 +0200

    laravel <> login auth  added

--- frontend/src/Contexts/AuthContext.jsx
+++ frontend/src/Contexts/AuthContext.jsx
@@ -3,5 +3,9 @@ import React, { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    
+   const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
 }
