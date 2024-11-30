cd6db3e0da1da92e7bb0104d10766c8990d6f08f
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-30 02:20:38 +0200

    laravel <> invitation.blade add

---
+++ backend/resources/views/invitation.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Collaboration Invitation</title>
+</head>
+<body>
+    <p>You have been invited to collaborate!</p>
+    <p>Click the link below to accept the invitation:</p>
+    <a href="{{ $invitationLink }}">Accept Invitation</a>
+</body>
+</html>
