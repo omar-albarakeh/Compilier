df79dba26f885a9f51d4b79a3c91f3ebde0d347a
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-01 01:53:26 +0200

    laravel <> Login Component usestate added

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Login = () => {
+
+   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, user } = useContext(AuthContext);
+
+  
   return (
     <div>Login</div>
   )
