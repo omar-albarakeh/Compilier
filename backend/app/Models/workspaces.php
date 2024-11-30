0f0d49438eb86abad05a2740554aebe263a7c5d1
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-30 02:26:10 +0200

    laravel <> workspaces model upated

--- backend/app/Models/workspaces.php
+++ backend/app/Models/workspaces.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class workspaces extends Model
 {
-    //
+    use HasFactory;
+
+   protected $fillable=
+   [
+        'name',
+        'users_id',
+    ];
 }
