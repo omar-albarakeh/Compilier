01d58cbac20f7f08a5171e528eb71c57bff0d067
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-30 02:47:22 +0200

    laravel <> InvitationController route added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvitationController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->post('/invitations/send',
+ [InvitationController::class, 'sendInvitation']);
