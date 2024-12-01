54afce3b8086e9c8116197eedc678e04a2f446ea
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-01 02:30:34 +0200

    laravel <> JWTAuthController register method added

--- backend/app/Http/Controllers/JWTAuthController.php
+++ backend/app/Http/Controllers/JWTAuthController.php
@@ -5,6 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class JWTAuthController extends Controller
-{
-    //
+{public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|max:255|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
 }
