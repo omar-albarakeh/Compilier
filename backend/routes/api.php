4c289c86e6f3ac5c1c3e28cbb8533f6ca6c44b46
omar albarakeh <omaralbarakeh2@gmail.com>
2024-12-01 02:51:58 +0200

    laravel <> api routes updated

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JWTAuthController;
+use App\Http\Middleware\JwtMiddleware;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => ['jwt']], function () {
+    Route::get('/user', [JWTAuthController::class, 'getUser']);
+    Route::post('/logout', [JWTAuthController::class, 'logout']);
+});
+
